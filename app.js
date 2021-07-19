adb8f47c9ba1b8287272b59e9ca391655ee17c2a
alan78814 <tri78814@gmail.com>
2021-07-19 22:15:56 +0800

    feat: add edit&new hbs

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const exphbs = require('express-handlebars')
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('edit')
 })
 app.listen(3000, () => {
     console.log(`App is running on http://localhost:${PORT}`)
