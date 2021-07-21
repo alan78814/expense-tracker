53145fe5a8d098fa782243a29500298d77693798
alan78814 <tri78814@gmail.com>
2021-07-21 22:55:05 +0800

    feat: route of index & new page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const PORT = 3000
 const exphbs = require('express-handlebars')
 const Record = require('./models/record')
+const db = require("./config/mongoose")
 const Category = require('./models/category')
 const helpers = require('handlebars-helpers')()
 
@@ -12,6 +13,7 @@ app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+
 //首頁
 app.get('/', async (req, res) => {
     const categories = await Category.find().lean()
@@ -32,7 +34,7 @@ app.get('/', async (req, res) => {
         .catch(error => console.error(error))
 })
 //首頁篩選
-app.get('/filter', async (req, res) => {
+app.get('/categories/filter', async (req, res) => {
     const categories = await Category.find().lean()
     const category = req.query.category
     if (!category) return res.redirect('/')
@@ -53,6 +55,26 @@ app.get('/filter', async (req, res) => {
             res.render('index', { records, categories, chosenRecord, totalAmount})
         })
         .catch(error => console.error(error))
+})
+// 首頁新增支出進入new.hbs
+app.get('/records/new', (req, res) => {
+    res.render('new')
+})
+// 新增支出頁送出
+app.post('/records', (req, res) => {
+    const record = req.body
+    const {name, category, date, amount} = record
+    return Record.create({ name, category, date, amount })
+    .then(() => res.redirect('/')) 
+    .catch(error => console.log(error))
+})
+// 首頁修改進入edit.hbs
+app.get('/records/edit', (req, res) => {
+    res.render('edit')
+})
+// edit.hbs送出資料
+app.post('/records/edit', (req, res) => {
+    res.render('edit')
 })
 
 app.listen(3000, () => {
