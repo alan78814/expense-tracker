ffdaf80d513b071c6c018c94d9afe1c5cc94810a
alan78814 <tri78814@gmail.com>
2021-08-10 16:47:32 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
+// 按下 login
 router.get('/login', (req, res) => {
   res.render('login')
 })
+
+// login 頁面送出資料
+router.post('/login', (req, res) => {
+})
+
+// 按下 register
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
 
 module.exports = router
