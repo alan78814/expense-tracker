3e61ad743a19e0a3d49fc24b5785970aabcfe112
alan78814 <tri78814@gmail.com>
2021-08-11 16:29:00 +0800

    feat: add user registration feature & errPage

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 // 按下 login
 router.get('/login', (req, res) => {
@@ -10,10 +11,41 @@ router.get('/login', (req, res) => {
 router.post('/login', (req, res) => {
 })
 
-// 按下 register
+// 按下 register 切換頁面
 router.get('/register', (req, res) => {
   res.render('register')
 })
 
+// register 頁面送出資料
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email })
+    .then(user => {
+      // 有註冊帳號，返回原本畫面
+      if (user) {
+        console.log('User already exists.')
+        res.render('register', {
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      } else {
+        // 如果還沒註冊：寫入資料庫
+        return User.create({
+          name,
+          email,
+          password
+        })
+
+      }
+
+    })
+    .then(() => res.redirect('/'))
+    .catch(err => {
+      console.log(err)
+      res.render('errorPage', { err }) //簡易錯誤面板，傳送 err 到使用者端
+    })
+})
 
 module.exports = router
