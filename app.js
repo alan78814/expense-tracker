237f348b9b88c153f1f7ee0d8187cd3eafc1452d
alan78814 <tri78814@gmail.com>
2021-08-14 11:29:30 +0800

    feat: add facebook login & dotenv

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const session = require('express-session')
 const usePassport = require('./config/passport')
 const flash = require('connect-flash')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
